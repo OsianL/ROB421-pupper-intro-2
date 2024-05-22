c320055b837b71d3d4adb6468425e15383a248b1
Trishula Dragon of the ice barrier <leahyo@oregonstate.edu>
2024-05-22 14:40:00 -0700

    moar testing 2

--- StanfordQuadrupedmini_pupper/src/Controller.py
+++ StanfordQuadrupedmini_pupper/src/Controller.py
@@ -57,9 +57,6 @@ class Controller:
             foot_location = state.foot_locations[:, leg_index]
             if contact_mode == 1:
                 new_location = self.stance_controller.next_foot_location(leg_index, state, command)
-                if (command.roll_step == True):
-                    print("goofy time")
-                    command.roll *= -1
             else:
                 swing_proportion = (
                     self.gait_controller.subphase_ticks(state.ticks) / self.config.swing_ticks
@@ -71,6 +68,12 @@ class Controller:
                     command
                 )
             new_foot_locations[:, leg_index] = new_location
+
+            #stupid hack to make the root giggle as it walks
+            if (command.roll_step == True and contact_modes[1] == 1):
+                    command.roll *= -1
+                    print(f"goofy time: {command.roll}")
+
         return new_foot_locations, contact_modes
 
 
