e62159efdd8b37cde0178c4fdff1b9747bb77623
Harold Barnett <barnetth@oregonstate.edu>
2024-05-28 10:38:43 -0700

    moveCirclesTest refactor

--- StanfordQuadrupedmini_pupper/moveCircleTest.py
+++ StanfordQuadrupedmini_pupper/moveCircleTest.py
@@ -101,6 +101,55 @@ def main():
     
     try:
         while True:
+            # 0 is default, I changed to 1 to get the back camera of my surface
+            cap = cv2.VideoCapture(0)  # Initialize webcam (0 is usually the default webcam)
+
+            # while True:
+            ret, frame = cap.read()  # Read a frame from the webcam
+
+            # if not ret:
+            #     # break
+            #     return None
+
+            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert frame to grayscale
+            # gray_blurred = cv2.GaussianBlur(gray, (5, 5), 0)  # Apply Gaussian blur to reduce noise
+
+            # cv2.imshow("grayscale", gray_blurred)
+
+            # circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, dp=1, minDist=50,
+            #                            param1=200, param2=30, minRadius=10, maxRadius=100)
+
+            # circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, dp=1, minDist=50,
+            #                            param1=200, param2=30, minRadius=10, maxRadius=100)
+
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv, (5, 120, 50), (25, 255, 255))
+            mask_blurred = cv2.GaussianBlur(mask, (9,9),0)
+
+            # cv2.imshow("orange mask", mask_blurred)
+
+            circles = cv2.HoughCircles(mask_blurred, cv2.HOUGH_GRADIENT, dp=1, minDist=50, 
+                                        param1=200, param2=30, minRadius=5, maxRadius=150) #min 10, max 100 default
+
+            # if circles is not None:
+            #     circles = np.round(circles[0, :]).astype("int")
+
+            #     for (x, y, r) in circles:
+            #         cv2.circle(frame, (x, y), r, (0, 255, 0), 4)  # Draw the circle
+            #         cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)  # Draw the center
+            #     print("Circles: ", circles)
+            # else: 
+
+
+            # cv2.imshow("Circle Tracker", frame)  # Display the frame
+
+            # if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
+            #     break
+
+            # cap.release()  # Release the webcam
+            # cv2.destroyAllWindows()  # Close all OpenCV windows
+            # return circles
+
             now = time.time()
             if now - last_loop < config.dt:
                 continue
@@ -114,6 +163,7 @@ def main():
             last_loop = time.time()
            
             circles = track_circles_in_webcam()
+            print(circles)
 
             if circles is None:
                 command.horizontal_velocity = np.array([0, 0])
